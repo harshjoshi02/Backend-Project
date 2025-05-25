fd8d0b85d0c047cb5982e70fd9eeb6f4f66b4117
harshjoshi02 <harshjoshi162003@gmail.com>
2025-05-25 15:46:34 +0530

    add user and video models

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import DB_NAME from "../constants.js";
 
-const connectDB = async () => {
+const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected!! DB Host: ${connectionInstance.connection.host}`);
+        console.log(`DB connected! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection error ", error);
+        console.log("mongoDB connection error", error);
         process.exit(1);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("error: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`the app is listening on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(`mongoDB connection error`);
+})
+
 
 
 
@@ -28,7 +42,7 @@ const app = express;
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         app.on("error", (error) => {
             console.log("error: ", error);
-            throw error;
+            throw err;
         })
 
         app.listen(process.env.PORT, () => {

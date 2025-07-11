71d9ee76252ceef19aafb3335d0b602c3416eb51
harshjoshi02 <harshjoshi162003@gmail.com>
2025-07-11 12:23:38 +0530

    added updateUser method in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -253,11 +253,38 @@ const getCurrentUser = asyncHandler( async() => {
     )
 })
 
+const updateUser = asyncHandler( async(req, res) => {
+    const {fullName, email} = req.body;
+
+    if(!fullName || !email) {
+        throw new ApiError(400, "all fields are required");
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                fullName,  // both ways are correct
+                email: email
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "accound details updated successfully")
+    )
+    
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser,
+    updateUser,
 };

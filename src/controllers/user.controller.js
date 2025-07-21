c5db27a3341a751396c3d11ac1e86c701a644009
harshjoshi02 <harshjoshi162003@gmail.com>
2025-07-21 13:54:09 +0530

    getWatchHistory user controller to be continued

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -419,6 +419,47 @@ const getUserChannelProfile = asyncHandler( async(req, res) => {
     );
 })
 
+const getWatchHistory = asyncHandler( async(req, res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: {
+                                $project: {
+                                    fullName: "true",
+                                    username: "true",
+                                    avatar: "true"
+                                }
+                            }
+                        },
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+})
+
 export {
     registerUser,
     loginUser,

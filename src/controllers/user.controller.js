754c468846f70d4eb57a628c918541a491f6326d
harshjoshi02 <harshjoshi162003@gmail.com>
2025-07-23 13:15:31 +0530

    logoutUser user controller bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -150,8 +150,8 @@ const logoutUser = asyncHandler( async(req, res) => {
     await User.findByIdAndDelete(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
